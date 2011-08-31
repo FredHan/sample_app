9871f106f2184befa72f050ad1a6ffacece19e20
Fred Han <hanfenglei@derbysoft.com>
2011-08-31 15:49:14 +0800

    User model validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,30 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {:name => "Example User",:email => "user@example.com"}
+  end
+  
+  it "should create a new instance given a valid attribute" do
+    User.create!(@attr)
+  end
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:name => ""))
+    no_name_user.should_not be_valid
+  end
+  
+  it "should require a email" do
+    no_email_user = User.new(@attr.merge(:email => ""))
+    no_email_user.should_not be_valid
+  end
+  
+  it "should reject invalid email addresses" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo]
+    addresses.each do |address|
+      invalid_email_user = User.new(@attr.merge(:email => address))
+      invalid_email_user.should_not be_valid
+    end
+  end
 end
 
 # == Schema Information
